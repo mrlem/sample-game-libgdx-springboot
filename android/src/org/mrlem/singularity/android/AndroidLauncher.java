e1fc4c6590ede86e31ff81599511fd76d5fc8bd2
Sebastien Guillemin <seb@mrlem.org>
2017-05-24 23:47:18 +0200

    initial commit

---
+++ android/src/org/mrlem/singularity/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.mrlem.singularity.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.mrlem.singularity.SingularityGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SingularityGame(), config);
+	}
+}
---
+++ desktop/src/org/mrlem/singularity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.mrlem.singularity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.mrlem.singularity.SingularityGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SingularityGame(), config);
+	}
+}
