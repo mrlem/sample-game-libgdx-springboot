3e4c9d4f93c57812a7d01c5a1e5e75e70d7c5cc9
Sebastien Guillemin <seb@mrlem.org>
2017-07-23 19:30:12 +0200

    client: use game/screen api

---
+++ client/core/src/org/mrlem/singularity/SampleScreen.kt
@@ -0,0 +1,36 @@
+package org.mrlem.singularity
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.ScreenViewport
+import ktx.app.KtxScreen
+
+class SampleScreen : KtxScreen {
+
+    val actor = SampleActor()
+    val stage = Stage(ScreenViewport())
+
+    init {
+        stage.addActor(actor)
+        Gdx.input.inputProcessor = stage
+
+        actor.setPosition(60f, 60f)
+    }
+
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
+
+    override fun render(delta: Float) {
+        Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        stage.act(delta)
+        stage.draw()
+    }
+
+    override fun dispose() {
+        stage.dispose()
+    }
+
+}
--- client/core/src/org/mrlem/singularity/SingularityGame.kt
+++ client/core/src/org/mrlem/singularity/SingularityGame.kt
@@ -1,41 +1,16 @@
 package org.mrlem.singularity
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.scenes.scene2d.Actor
-import com.badlogic.gdx.scenes.scene2d.Stage
-import ktx.app.KotlinApplication
-import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.badlogic.gdx.Screen
+import ktx.app.KtxGame
 
-class SingularityGame : KotlinApplication() {
-
-    lateinit var stage: Stage
-    lateinit var actor: Actor
+class SingularityGame : KtxGame<Screen>() {
 
     override fun create() {
         Gdx.app.log("app", "launching client...")
 
-        actor = SampleActor()
-
-        stage = Stage(ScreenViewport())
-        stage.addActor(actor)
-        Gdx.input.inputProcessor = stage
-
-        actor.setPosition(60f, 60f)
+        addScreen(SampleScreen())
+        setScreen<SampleScreen>()
     }
 
-    override fun resize(width: Int, height: Int) {
-        stage.viewport.update(width, height, true)
-    }
-
-    override fun render(delta: Float) {
-        Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        stage.act(delta)
-        stage.draw()
-    }
-
-    override fun dispose() {
-        stage.dispose()
-    }
 }
